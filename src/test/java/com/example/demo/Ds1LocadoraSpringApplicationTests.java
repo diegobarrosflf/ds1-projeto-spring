0ccfb58b4f5c805ef6bec0be62be586e00cad465
Diego Barros <diegobarrosflf@gmail.com>
2020-06-10 19:46:48 -0300

    Criar o projeto

---
+++ src/test/java/com/example/demo/Ds1LocadoraSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ds1LocadoraSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
